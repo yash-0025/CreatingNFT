5a5e5b6ecef08c23cbb6d75469e7b09dd3c76b07
Yash Patel <yashpatel200403@gmail.com>
2022-03-18 15:34:29 +0530

    Hardhat Installed Basic starter project

---
+++ hardhat/contracts/myNFT.sol
@@ -0,0 +1,10 @@
+//  SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzepplin/contracts/token/ERC721/ERC721.sol";
+import "@openzepplin/contracts/utils/Counters.sol";
+import "@openzepplin/contracts/access/Ownable.sol";
+import "@openzepplin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+
